05ecf5842956e6f9caee103cf7f2966e06a934bf
bad49wolf <bad49wolf@gmail.com>
2021-11-08 09:06:02 -0500

    Add files via upload

---
+++ FunWithLayoutsTP/cpp/dialog3.cpp
@@ -0,0 +1,36 @@
+#include "hpp/dialog3.h"
+
+Dialog3::Dialog3(QWidget *parent):QWidget(parent){
+createWidgets();
+placeWidgets();
+}
+
+ void Dialog3::createWidgets(){
+   grid_layout = new QGridLayout;
+   nums={ "9", "8", "7","/","6", "5", "4","*","3", "2", "1","+","0", "-", ".", "=", };
+   lcd = new QLCDNumber;
+   lcd->setSegmentStyle(QLCDNumber::Filled);
+}
+
+void Dialog3::placeWidgets(){
+    layout = new QVBoxLayout();
+    layout->setSpacing(2);
+    layout->addWidget(lcd);
+    layout->addLayout(grid_layout);
+    int pos = 0;
+    for (int i=0; i<4; i++) {
+     for (int j=0; j<4; j++) {
+       auto *button = new QPushButton(nums[pos], this);
+       button->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+       grid_layout->addWidget(button, i, j);
+       pos++;
+     }
+    }
+      setLayout(layout);
+  }
+
+
+
+
+
+
